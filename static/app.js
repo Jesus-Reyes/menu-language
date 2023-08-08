17d9cda608130bbe22fa805163e97ec5cc7f258f
Jesus-Reyes <chuchin43@outlook.com>
2023-08-08 07:01:38 -0600

    template v0.2

--- static/app.js
+++ static/app.js
@@ -1,32 +1,49 @@
-document.getElementById('language-select').addEventListener('change', function() {
-    const iframe = document.getElementById('scorm-frame');
-    const languageCard = document.getElementById('language-card');
-    const backButton = document.getElementById('back-button');
+const column1 = document.getElementById('column1');
+const column2 = document.getElementById('column2');
+const startButton = document.querySelector('.start-button');
 
-    if (this.value !== '') {
-        if (this.value === 'es') {
-            window.location.href = './scorms/scorm-es/index.html';
-        } else if (this.value === 'en') {
-            window.location.href = './scorms/scorm-en/index.html';
-        } else if (this.value === 'fr') {
-            window.location.href = './scorms/scorm-fr/index.html';
-        }
+const languages = [
+    { display: "English", native: "English" },
+    { display: "French", native: "Français" },
+    { display: "German", native: "Deutsch" },
+    { display: "Indonesian", native: "Bahasa Indonesia" },
+    { display: "Italian", native: "Italiano" },
+    { display: "Japanese", native: "日本語" },
+    { display: "Polish", native: "Polski" },
+    { display: "Portuguese", native: "Português" },
+    { display: "Russian", native: "Русский" },
+    { display: "Spanish", native: "Español" },
+    { display: "Turkish", native: "Türkçe" }
+];
+
+languages.forEach((lang, index) => {
+    const li = document.createElement('li');
+    li.setAttribute('data-language', lang.display);
+    li.innerHTML = `
+        <i class="check-icon">&#10003;</i>
+        <span>${index + 1}. ${lang.display} - ${lang.native}</span>
+    `;
+
+    if (index < 6) {
+        column1.appendChild(li);
+    } else {
+        column2.appendChild(li);
     }
-
-
 });
 
+// Deshabilitar el botón "Start" en la carga inicial
+startButton.setAttribute('disabled', 'disabled');
 
-document.getElementById('back-button').addEventListener('click', function() {
-    const iframe = document.getElementById('scorm-frame');
-    const languageCard = document.getElementById('language-card');
-    const languageSelect = document.getElementById('language-select');
+document.querySelectorAll('.language-list li').forEach(item => {
+    item.addEventListener('click', function() {
+        const previouslySelected = document.querySelector('.selected');
+        if (previouslySelected) {
+            previouslySelected.classList.remove('selected');
+            previouslySelected.querySelector('.check-icon').style.display = 'none';
+        }
 
-    iframe.style.display = 'none';
-    languageCard.style.display = 'block';
-    this.style.display = 'none';
-
-    // Reset the language selection to the empty option
-    languageSelect.selectedIndex = 0;
-    iframe.src = '';
+        this.classList.add('selected');
+        this.querySelector('.check-icon').style.display = 'inline';
+        startButton.removeAttribute('disabled');
+    });
 });
