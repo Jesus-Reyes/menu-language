52c933678b804ec80aa6501605752e2ead37392f
Jesus-Reyes <chuchin43@outlook.com>
2023-08-22 19:42:07 -0600

    Multiples idiomas - falta ingles

--- static/app.js
+++ static/app.js
@@ -3,18 +3,19 @@ const column2 = document.getElementById('column2');
 const startButton = document.querySelector('.start-button');
 
 const languages = [
-    { display: "English", native: "English" },
-    { display: "French", native: "Français" },
-    { display: "German", native: "Deutsch" },
-    { display: "Indonesian", native: "Bahasa Indonesia" },
-    { display: "Italian", native: "Italiano" },
-    { display: "Japanese", native: "日本語" },
-    { display: "Polish", native: "Polski" },
-    { display: "Portuguese", native: "Português" },
-    { display: "Russian", native: "Русский" },
-    { display: "Spanish", native: "Español" },
-    { display: "Turkish", native: "Türkçe" }
+    { display: "English", native: "English", url: "SCORM-INGLES" },
+    { display: "French", native: "Français", url: "SCORM-FRANCES" },
+    { display: "German", native: "Deutsch", url: "SCORM-ALEMAN" },
+    { display: "Indonesian", native: "Bahasa Indonesia", url: "SCORM-INDONESIO" },
+    { display: "Italian", native: "Italiano", url: "SCORM-ITALIANO" },
+    { display: "Japanese", native: "日本語", url: "SCORM-JAPONES" },
+    { display: "Polish", native: "Polski", url: "SCORM-POLACO" },
+    { display: "Portuguese", native: "Português", url: "SCORM-PORTUGUES" },
+    { display: "Russian", native: "Русский", url: "SCORM-RUSO" },
+    { display: "Spanish", native: "Español", url: "SCORM-ESPANOL" },
+    { display: "Turkish", native: "Türkçe", url: "SCORM-TURCO" }
 ];
+
 
 languages.forEach((lang, index) => {
     const li = document.createElement('li');
@@ -45,5 +46,21 @@ document.querySelectorAll('.language-list li').forEach(item => {
         this.classList.add('selected');
         this.querySelector('.check-icon').style.display = 'inline';
         startButton.removeAttribute('disabled');
+
     });
+});
+
+
+startButton.addEventListener('click', function() {
+    // Encuentra el idioma seleccionado
+    const selectedLangElement = document.querySelector('.selected');
+    if (!selectedLangElement) return;  // No hacer nada si no hay idioma seleccionado
+
+    const langKey = selectedLangElement.getAttribute('data-language');
+    const selectedLang = languages.find(lang => lang.display === langKey);
+
+    if (!selectedLang || !selectedLang.url) return;  // No hacer nada si el idioma seleccionado no tiene una URL
+
+    // Redirige a la URL correspondiente del idioma seleccionado
+    window.location.href = `./scorms/${selectedLang.url}/index_scorm.html`;
 });
